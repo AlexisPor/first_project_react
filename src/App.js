04ec5a805536c1499b80fafa8b3cc2b81ef6d727
Alexis Por <alexis19971712@gmail.com>
2021-06-05 14:37:57 +0200

    Ajout composant Card + Card design

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import ExpenseItem from "./components/ExpenseItem";
 import './App.css';
 import Expenses from "./components/Expenses";
 
@@ -7,32 +6,32 @@ function App() {
     const expenses = [
         {
             id: 'e1',
-            title: 'Toilet Paper',
+            title: 'Jouet',
             amount: 94.12,
             date: new Date(2020, 7, 14),
         },
         {
             id: 'e2',
-            title: 'New TV',
+            title: 'Nouvelle Tv',
             amount: 799.49,
             date: new Date(2021, 2, 12)
         },
         {
             id: 'e3',
-            title: 'Car Insurance',
+            title: 'Assurance Voiture',
             amount: 294.67,
             date: new Date(2021, 2, 28),
         },
         {
             id: 'e4',
-            title: 'New Desk (Wooden)',
+            title: 'Nouveau bureau',
             amount: 450,
             date: new Date(2021, 5, 12),
         },
     ];
 
     return (
-        <div className="App">
+        <div className="expenses">
             <Expenses items={expenses}/>
         </div>
     );
