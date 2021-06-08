ddd27c35f0735a6f252ed257fad6419b6db327b2
Alexis <alexis19971712@gmail.com>
2021-06-08 23:50:07 +0200

    passage de donnée d'un composant enfant à parent

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -16,7 +16,7 @@ const ExpenseItem = (props) => {
         <Card className="expense-item">
             <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
-                <h2>{props.date}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">{props.amount}€</div>
             </div>
             <button onClick={clickHandler} >Change Title</button>
